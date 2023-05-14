04000c29dc67fc8d3495c6f6bce1f2a321bbc652
woaitsAryan <yoprince.ab@gmail.com>
2023-05-14 22:18:48 +0530

    logic done

--- logic.cpp
+++ logic.cpp
@@ -1,33 +1,101 @@
 #include <iostream>
-#include <string.h>
+#include <cstring>
+
+#define MAX 9
 
 using namespace std;
 
-typedef struct{
+typedef struct
+{
     int winner;
     int loser;
-}pairr;
+}
+arrpair;
 
-int main(int argc, string argv[] ){
-    int preferences[10][10];
-    bool locked[10][10];
-    pairr swap[0];
-    pairr pairs[40];
+int preferences[MAX][MAX];
+bool locked[MAX][MAX];
+string candidates[MAX];
+arrpair arrpairs[MAX * (MAX - 1) / 2];
 
-    string candidates[argc - 1];
+int arrpair_count;
+int candidate_count;
 
-    for(int i = 1; i < argc; i++){
-        candidates[i - 1] = argv[i];
+bool vote(int rank, string name, int ranks[]);
+void record_preferences(int ranks[]);
+void add_arrpairs();
+void sort_arrpairs();
+void lock_arrpairs();
+void print_winner();
+bool lock_check(int ii, int jj);
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        cout << "Usage: tideman [candidate ...]" << endl;
+        return 1;
     }
-    
 
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        cout << "Maximum number of candidates is " << MAX << endl;
+        return 2;
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i] = argv[i + 1];
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            locked[i][j] = false;
+        }
+    }
+
+    arrpair_count = 0;
+    int voter_count;
+    cout << "Number of voters: ";
+    cin >> voter_count;
+
+    for (int i = 0; i < voter_count; i++)
+    {
+        int ranks[candidate_count];
+
+        for (int j = 0; j < candidate_count; j++)
+        {
+            cout << "Rank " << j + 1 << ": ";
+            string name;
+            cin >> name;
+
+            if (!vote(j, name, ranks))
+            {
+                cout << "Invalid vote." << endl;
+                return 3;
+            }
+        }
+
+        record_preferences(ranks);
+
+        cout << endl;
+    }
+
+    add_arrpairs();
+    sort_arrpairs();
+    lock_arrpairs();
+    print_winner();
+    return 0;
 }
 
-bool vote(int rank, string name, int ranks[], int count, string candidates[]){
-
-    for(int i = 0; i < count; i++){
-        
-        if(candidates[i] == name){
+bool vote(int rank, string name, int ranks[])
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (name == candidates[i])
+        {
             ranks[rank] = i;
             return true;
         }
@@ -35,98 +103,111 @@ bool vote(int rank, string name, int ranks[], int count, string candidates[]){
     return false;
 }
 
-void record_preferences(int ranks[], int count, int preferences[10][10]){
-
-    for(int i = 0; i < count; i++){
-
-        for(int j = i + 1; j < count; j++){
-
+void record_preferences(int ranks[]){
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
             preferences[ranks[i]][ranks[j]]++;
         }
     }
-    return;
 }
 
-void add_pairs(int count, int preferences[10][10], int paircount, pairr pairs[]){
-    
-    for(int i = 0; i< count; i++){
-        
-        for(int j = 0; j < count; j++){
-            if(preferences[i][j] > preferences[j][i]){
-                pairs[paircount].winner = i;
-                pairs[paircount].loser = j;
-                paircount++;
+void add_arrpairs()
+{
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                arrpairs[arrpair_count].winner = i;
+                arrpairs[arrpair_count].loser = j;
+                arrpair_count++;
             }
-            else if(preferences[i][j] < preferences[j][i]){
-                pairs[paircount].winner = j;
-                pairs[paircount].loser = i;
-                paircount++;
-            }
-        }
-    }
-    return;
-}
-
-void sortpairs(pairr pairs[], int paircount, int preferences[10][10]){
-
-    for(int i = 0; i < paircount; i++){
-
-        for(int j = 0; j < paircount - i - 1; j++){
-
-            int temp = pairs[j].winner;
-            int temp2 = pairs[j].loser;
-
-            int firstpairstrength = preferences[temp][temp2] - preferences[temp2][temp];
-
-            int temp3 = pairs[j + 1].winner;
-            int temp4 = pairs[j + 1].loser;
-
-            int secondpairstrength = preferences[temp3][temp4] - preferences[temp4][temp3];
-
-            if(firstpairstrength < secondpairstrength){
-                
-                swap(pairs[j], pairs[j + 1]);
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                arrpairs[arrpair_count].winner = j;
+                arrpairs[arrpair_count].loser = i;
+                arrpair_count++;
             }
         }
     }
-    return;
 }
 
-void lockpairs(int paircount, pairr pairs[], bool locked[10][10], int count){
+void sort_arrpairs()
+{
+    for (int i = 0; i < arrpair_count; i++)
+    {
+        for (int j = 0; j < arrpair_count - i - 1; j++)
+        {
+            int i1 = arrpairs[j].winner;
+            int j1 = arrpairs[j].loser;
+            int strength_arrpair1 = preferences[i1][j1] - preferences[j1][i1];
 
-    for(int i = 0; i < paircount; i++){
+            int i2 = arrpairs[j + 1].winner;
+            int j2 = arrpairs[j + 1].loser;
+            int strength_arrpair2 = preferences[i2][j2] - preferences[j2][i2];
 
-        int temp = pairs[i].winner;
-        int temp2 = pairs[i].loser;
-
-        locked[temp][temp2] = true;
-
-        if(checklock(temp, temp2, count, locked)){
-            locked[temp2][temp] = false;
+            if (strength_arrpair1 < strength_arrpair2)
+            {
+                swap(arrpairs[j], arrpairs[j+1]);
+            }
         }
-
     }
-    return;
 }
 
-bool checklock(int temp, int temp2, int count, bool locked[10][10]){
-    if(count == 1){
+void lock_arrpairs()
+{
+    for (int i = 0; i < arrpair_count; i++)
+    {
+        int ii = arrpairs[i].winner;
+        int jj = arrpairs[i].loser;
+        locked[ii][jj] = true;
+
+        if (lock_check(ii, jj))
+        {
+            locked[ii][jj] = false;
+        }
+    }
+}
+
+void print_winner()
+{
+    for (int j = 0; j < candidate_count; j++)
+    {
+        int counter1 = 0;
+
+        for (int i = 0; i < candidate_count; i++)
+        {
+            if (!locked[i][j])
+            {
+                counter1++;
+                if (counter1 == candidate_count)
+                {
+                    cout << candidates[j] << endl;
+                }
+            }
+        }
+    }
+}
+
+bool lock_check(int ii, int jj)
+{
+    if (candidate_count == 1)
         return false;
-    }
 
-    if( temp == temp2){
+    if (ii == jj)
         return true;
-    }
 
-    for(int i = 0; i < count; i++){
-
-        if(locked[temp2][i] == true){
-
-            if(checklock(temp, i, count, locked)){
-
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (locked[jj][i])
+        {
+            if (lock_check(ii, i))
                 return true;
-            }
         }
     }
+
     return false;
 }
